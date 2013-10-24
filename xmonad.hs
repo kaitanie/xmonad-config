c59002f5a264e51cdf1d1e04cad7904ef5157ea0
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2013-10-24 21:25:12 +0300

    Use unison-gtk for graphical feedback

--- xmonad.hs
+++ xmonad.hs
@@ -338,7 +338,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask .|.  shiftMask, xK_p ), spawn "kopete")
     , ((modMask .|.  shiftMask, xK_b ), spawn "google-chrome")
     , ((modMask .|.  shiftMask, xK_x ), spawn "xterm")
-    , ((modMask .|.  shiftMask, xK_s ), spawn "unison -terse -batch keep")
+    , ((modMask .|.  shiftMask, xK_s ), spawn "unison-gtk -terse -batch keep")
     , ((modMask, xK_z ),                spawn "emacs")
 
     --Keyboard layout
